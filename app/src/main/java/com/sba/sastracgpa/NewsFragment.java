34cf8b842e10149ef6837fafab4fb3058394a86e
Saif Basheer <saif191020@gmail.com>
2020-08-08 12:58:16 +0530

    Bug-Fix with News Fetching

--- app/src/main/java/com/sba/sastracgpa/NewsFragment.java
+++ app/src/main/java/com/sba/sastracgpa/NewsFragment.java
@@ -1,14 +1,13 @@
 package com.sba.sastracgpa;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,29 +18,35 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class NewsFragment extends Fragment {
 
     private RecyclerView recyclerView;
 
-    String Title[] =new String[15];
-    String Dates[] =new String[15];
-    String Links[] =new String[15];
+    String[] Title = new String[15];
+    String[] Dates = new String[15];
+    String[] Links = new String[15];
+    private static final String TAG = "NEWS FARG";
     ProgressDialog progressDialog;
-    int itemCount  =0;
-    public static final String LOGTAG ="com.sba.MyLog";
+    int itemCount = 0;
+    public static final String LOGTAG = "com.sba.MyLog";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view =inflater.inflate(R.layout.newsfragment,container,false);
-        recyclerView=view.findViewById(R.id.news_Recycler);
+        View view = inflater.inflate(R.layout.newsfragment, container, false);
+        recyclerView = view.findViewById(R.id.news_Recycler);
         new Newses().execute();
 
         return view;
     }
-    public class Newses extends AsyncTask<Void,Void,Void>{
+
+    @SuppressLint("StaticFieldLeak")
+    public class Newses extends AsyncTask<Void, Void, Void> {
+
 
         @Override
         protected void onPreExecute() {
@@ -50,9 +55,6 @@ public class NewsFragment extends Fragment {
             progressDialog.setMessage("Loding Content...");
             progressDialog.show();
             progressDialog.setCancelable(false);
-
-
-
         }
 
         @Override
@@ -70,19 +72,38 @@ public class NewsFragment extends Fragment {
                 }
 */
                 final Document document = Jsoup.connect("https://www.sastra.edu/running-news.html").get();
-                Log.i(LOGTAG,"Connected");
-                int i=-1;
-                for(Element row : document.select("table.table.table-striped.table-bordered.table-hover tr"))
-                {
-                    if(++i ==0)
+                Log.i(LOGTAG, "Connected");
+                int i = -1;
+                for (Element row : document.select("table.table.table-striped.table-bordered.table-hover tr")) {
+                    if (++i == 0)
                         continue;
                     Title[itemCount] = row.select(".list-title a").text();
-                    Dates[itemCount] = (row.select(".list-title").text()).replace(Title[itemCount]," ").trim();
+                    Dates[itemCount] = (row.select(".list-title").text()).replace(Title[itemCount], " ").trim();
                     Links[itemCount++] = (row.select("a").first()).attr("abs:href");
-                    Log.i(LOGTAG,Dates[itemCount-1]);
+                    Log.i(LOGTAG, Dates[itemCount - 1]);
                 }
-            }catch (Exception e)
-            {
+            } catch (org.jsoup.HttpStatusException e) {
+                if (e.getStatusCode() == 404) {
+                    final Document document;
+                    try {
+                        document = Jsoup.connect("https://www.sastra.edu/home/running-news.html").get();
+
+                        Log.i(LOGTAG, "Connected");
+                        int i = -1;
+                        for (Element row : document.select("table.table.table-striped.table-bordered.table-hover tr")) {
+                            if (++i == 0)
+                                continue;
+                            Title[itemCount] = row.select(".list-title a").text();
+                            Dates[itemCount] = (row.select(".list-title").text()).replace(Title[itemCount], " ").trim();
+                            Links[itemCount++] = (row.select("a").first()).attr("abs:href");
+                            Log.i(LOGTAG, Dates[itemCount - 1]);
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
@@ -91,9 +112,9 @@ public class NewsFragment extends Fragment {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            NewsAddapter newsAddapter =new NewsAddapter(getActivity(),Title,Links,Dates,itemCount);
+            NewsAddapter newsAddapter = new NewsAddapter(getActivity(), Title, Links, Dates, itemCount);
             progressDialog.dismiss();
-            Log.i(LOGTAG,itemCount+"");
+            Log.i(LOGTAG, itemCount + "");
             recyclerView.setAdapter(newsAddapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         }
